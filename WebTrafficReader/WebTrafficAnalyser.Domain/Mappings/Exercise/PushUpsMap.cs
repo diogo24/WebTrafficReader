ad37fe3e684afc92cead599d2c6222c684e22bab
Diogo24 <diocar24@hotmail.com>
2015-09-02 17:52:48 +0100

    WARNING: DATABASE ACCESS NOT WORKING YET!!!
    - Database SQL (other oprtions MySql or MariaDB)
    - ORMs NHibernate (other option Entity Framework)
    
    - http://gergroen.blogspot.co.uk/2011/11/nhibernate-getting-started-guide.html

---
+++ WebTrafficReader/WebTrafficAnalyser.Domain/Mappings/Exercise/PushUpsMap.cs
@@ -0,0 +1,21 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebTrafficAnalyser.Domain.Models.Exercise;
+
+namespace WebTrafficAnalyser.Domain.Mappings.Exercise
+{
+    public class PushUpsMap: ClassMapping<PushUps>
+    {
+        public PushUpsMap()
+        {
+            Id(x => x.ID, m => m.Generator(Generators.Native));
+            Property(x => x.Repetitions);
+            Property(x => x.Time);
+        }
+    }
+}
---
+++ WebTrafficReader/WebTrafficAnalyser.Domain/Models/Exercise/PushUps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTrafficAnalyser.Domain.Models.Exercise
+{
+    public class PushUps
+    {
+        public virtual int ID { get; set; }
+        public virtual int Repetitions { get; set; }
+        public virtual DateTime Time { get; set; }
+    }
+}
