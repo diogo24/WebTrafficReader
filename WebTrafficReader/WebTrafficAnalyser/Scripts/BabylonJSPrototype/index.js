047f1f8c0ff445998f109fb88bea2d44cac2ee72
Diogo24 <diocar24@hotmail.com>
2015-10-13 20:01:18 +0100

    BabylonJSPrototype example.

--- WebTrafficReader/WebTrafficAnalyser/Scripts/BabylonJSPrototype/index.js
+++ WebTrafficReader/WebTrafficAnalyser/Scripts/BabylonJSPrototype/index.js
@@ -1,26 +1,47 @@
 ﻿///<reference path="scripts/babylon.max.js" />
 
-var createScene = function () {
-    // Get the canvas element from our HTML below
-    var canvas = document.getElementById("renderCanvas");
+var createScene = function (engine, canvas) {
+    //BABYLON.SceneLoader.Load("Assets/Spaceship/", "spaceship.babylon", engine, function (scene) {
 
-    // Load the BABYLON 3D engine
-    var engine = new BABYLON.Engine(canvas, true);
+    //    //attach Camera
+    //    scene.activeCamera.attachControl(canvas);
 
-    BABYLON.SceneLoader.Load("Assets/Spaceship/", "spaceship.babylon", engine, function (scene) {
+    //    // Register a render loop to repeatedly render the scene
+    //    engine.runRenderLoop(function () {
+    //        scene.render();
+    //    });
 
-        //attach Camera
-        //scene.activeCamera.attachControl(canvas);
+    //}, null, null);
 
-        // Register a render loop to repeatedly render the scene
-        engine.runRenderLoop(function () {
-            scene.render();
-        });
+    //// Watch for browser/canvas resize events
+    //window.addEventListener("resize", function () {
+    //    engine.resize();
+    //});
 
-    }, null, null);
+    // Now create a basic Babylon Scene object 
+    var scene = new BABYLON.Scene(engine);
 
-    // Watch for browser/canvas resize events
-    window.addEventListener("resize", function () {
-        engine.resize();
-    });
+    // Change the scene background color to green.
+    scene.clearColor = new BABYLON.Color3(0, 1, 0);
+
+    // This creates and positions a free camera
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // This attaches the camera to the canvas
+    //camera.attachControl(canvas, false);
+
+    // This creates a light, aiming 0,1,0 - to the sky.
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Dim the light a small amount
+    light.intensity = .5;
+
+    // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
+    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 10, 1, scene);
+
+    // Leave this function
+    return scene;
 }
